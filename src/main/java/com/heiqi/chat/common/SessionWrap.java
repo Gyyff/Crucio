301b3257d0fe95d15fb7dca96aa427c2229c9705
Gyff <1821661414@qq.com>
2023-09-21 20:26:28 +0800

    1

---
+++ src/main/java/com/heiqi/chat/common/SessionWrap.java
@@ -0,0 +1,15 @@
+package com.heiqi.chat.common;
+
+import jakarta.websocket.Session;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SessionWrap {
+    private int from;     //会话发起者id
+    private int to;     //会话接收者id
+    private Session session;
+
+
+}
