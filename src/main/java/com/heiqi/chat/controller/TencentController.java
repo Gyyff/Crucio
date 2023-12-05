5410fd5ac720330874ec385f7aa9c40fb93a4588
zhouly <bonies@126.com>
2023-12-05 15:09:52 +0800

    获取api签名

--- src/main/java/com/heiqi/chat/controller/TencentController.java
+++ src/main/java/com/heiqi/chat/controller/TencentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description
  */
 @RestController
-@RequestMapping("/tencent")
+@RequestMapping("/api/tencent")
 public class TencentController {
 
   @Autowired
