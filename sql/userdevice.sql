6a8615a45cbc284804c2c568b89d6c50ad38a126
Bonies <Bonies@126.com>
2023-12-10 23:03:05 +0800

    用户设备

--- sql/userdevice.sql
+++ sql/userdevice.sql
@@ -1,32 +1,9 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : chat
- Source Server Type    : MySQL
- Source Server Version : 50718
- Source Host           : gz-cdb-n80gj0zv.sql.tencentcdb.com:63547
- Source Schema         : chat
-
- Target Server Type    : MySQL
- Target Server Version : 50718
- File Encoding         : 65001
-
- Date: 07/12/2023 11:05:18
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for userdevice
--- ----------------------------
 DROP TABLE IF EXISTS `userdevice`;
-CREATE TABLE `userdevice`  (
-  `Id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `UserId` int(11) NULL DEFAULT NULL COMMENT '用户id',
-  `DeviceId` varchar(64) NULL DEFAULT NULL COMMENT '设备id',
-  PRIMARY KEY (`Id`) USING BTREE,
-  UNIQUE INDEX `idx_userid`(`UserId`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3;
-
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `userdevice` (
+                              `Id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                              `UserId` int(11) NOT NULL COMMENT '用户id',
+                              `DeviceId` varchar(128) NOT NULL COMMENT '设备id',
+                              `ClientType` tinyint(2) NOT NULL DEFAULT '0' COMMENT '0-安卓 1-ios ',
+                              PRIMARY KEY (`Id`),
+                              UNIQUE KEY `idx_userid` (`UserId`) USING BTREE
+)
